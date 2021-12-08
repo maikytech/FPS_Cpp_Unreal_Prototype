9e0f721168991226fb1f8e90b8a7f81e0b82b36c
maikytech <maikytech1307@gmail.com>
2021-12-07 19:44:55 -0500

    Initial commit

---
+++ Source/FPS_Prototype/FPS_PrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPS_PrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_PROTOTYPE_API AFPS_PrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
